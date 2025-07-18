bf13d6a7004cd7871783f1e68b0c255e4b1dc220
Rhushiraj Mehta <rhushirajigo@gmail.com>
2025-07-18 11:30:24 +0000

    new meta pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,45 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
 import Head from 'next/head';
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Osho Wisdom Vault',
-  description: "Osho's All Life Learnings at One Place",
+  title: 'Your Site Title',
+  description: 'Short site description',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth !scroll-smooth">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16526190681"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'AW-16526190681');
-          `}
-        </script>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased tracking-wide leading-relaxed">
+        {/* Meta Pixel Script */} 
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){
+              n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1437087947631378');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* Meta Pixel Fallback Image for NoScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1437087947631378&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+          />
+        </noscript>
+      </Head>
+      <body>
         {children}
         <Toaster />
       </body>
