13da80bf242a806813969152faf2577d4ec894a9
Rhushiraj Mehta <rhushirajigo@gmail.com>
2025-07-18 11:24:07 +0000

    meta pixel code added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
+import Head from 'next/head';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
@@ -13,9 +14,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <head>
+    <html lang="en" className="scroll-smooth !scroll-smooth">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16526190681"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-16526190681');
+          `}
+        </script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
